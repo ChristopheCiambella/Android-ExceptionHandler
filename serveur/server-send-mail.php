47b320c596816d7b11a5ad2ec1815d1fe8e6dc90
Christophe Ciambella <christophe.ciambella@gmail.com>
2014-12-11 12:46:32 +0100

    Add phone model and Android version to stack trace

--- serveur/server-send-mail.php
+++ serveur/server-send-mail.php
@@ -1,11 +1,19 @@
 <?php
-        if ( $_POST['stacktrace'] == "" || $_POST['package_version'] == "" || $_POST['package_name'] == "" ) {
-                die("This script is used to collect field test crash stacktraces. No personal information is transmitted, collected or stored.<br/>For more information, please contact <a href='mailto:support@nullwire.com'>email@domain.com</a>");
-        }
+if ( $_POST['stacktrace'] == "" || $_POST['package_version'] == "" || $_POST['package_name'] == "" ) 
+{
+	die("This script is used to collect field test crash stacktraces. No personal information is transmitted, collected or stored.<br/>For more information, please contact <a href='mailto:support@nullwire.com'>email@domain.com</a>");
+}
 		
-        $random = rand(1000,9999);
-        $version = $_POST['package_version'];
-        $package = $_POST['package_name'];
+// Extract data
+$version = $_POST['package_version'];
+$package = $_POST['package_name'];
+$phone_model = $_POST['phone_model'];
+$android_version = $_POST['android_version'];
+$stacktrace = $_POST['stacktrace'];
 
-        mail("mads.kristiansen@nullwire.com", "IMPORTANT: Exception received (".$version.")", $_POST['stacktrace'], "from:bugs@nullwire.com");
+// Build stack trace report
+$message = 'PHONE MODEL => '. $phone_model ."\nANDROID VERSION => ". $android_version ."\n\n". $stacktrace;
+
+// Send mail
+mail("mads.kristiansen@nullwire.com", "IMPORTANT: Exception received (". $version .")", $message, "from:bugs@nullwire.com");
 ?>
--- serveur/server-write-file.php
+++ serveur/server-write-file.php
@@ -1,12 +1,22 @@
 <?php
-        if ( $_POST['stacktrace'] == "" || $_POST['package_version'] == "" || $_POST['package_name'] == "" ) {
-                die("This script is used to collect field test crash stacktraces. No personal information is transmitted, collected or stored.<br/>For more information, please contact <a href='mailto:support@nullwire.com'>email@domain.com</a>");
-        }
-        $random = rand(1000,9999);
-        $version = $_POST['package_version'];
-        $package = $_POST['package_name'];
-        $handle = fopen($package."-trace-".$version."-".time()."-".$random, "w+");
-        fwrite($handle, $_POST['stacktrace']);
-        fclose($handle);
+if ( $_POST['stacktrace'] == "" || $_POST['package_version'] == "" || $_POST['package_name'] == "" ) 
+{
+	die("This script is used to collect field test crash stacktraces. No personal information is transmitted, collected or stored.<br/>For more information, please contact <a href='mailto:support@nullwire.com'>email@domain.com</a>");
+}
+		
+// Extract data
+$version = $_POST['package_version'];
+$package = $_POST['package_name'];
+$phone_model = $_POST['phone_model'];
+$android_version = $_POST['android_version'];
+$stacktrace = $_POST['stacktrace'];
 
+// Build stack trace report
+$message = 'PHONE MODEL => '. $phone_model ."\nANDROID VERSION => ". $android_version ."\n\n". $stacktrace;
+
+// Write file
+$random = rand(1000, 9999);
+$handle = fopen($package."-trace-".$version."-".time()."-".$random, "w+");
+fwrite($handle, $message);
+fclose($handle);
 ?>
